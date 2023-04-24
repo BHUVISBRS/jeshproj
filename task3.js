e88154e11b42a7e8980fcb3c3d5e3f6b91cd904d
BHUVISBRS <bhuvi@sbrs@gmail.com>
2023-04-25 00:56:37 +0530

    second commit

--- task3.js
+++ task3.js
@@ -2,4 +2,9 @@
 
 var headerTitle =document.getElementById('header-title');
 var header =document.getElementById('main-header');
-header.style.borderBottom ='solid 10px #000'; 
+header.style.borderBottom ='solid 10px #000';
+
+
+var items= document.getElementByIdClassName('list-group-item');
+console.log(items[4]);
+items[3].style.fontWeight ='yellow';
